8d9e679ede7fd722ea0280fed3ca0e60432ac379
gmontoyam <118438539+gmontoyam@users.noreply.github.com>
2022-11-17 11:38:19 +0100

    Update streamlit_app.py
    content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner') 
+
+streamlit.header('Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('kales, spinach & rocket smmothie')
+streamlit.text('Hard boiled Free-ranged egg')
